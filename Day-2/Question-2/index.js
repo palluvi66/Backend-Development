d1a6da3bd29e2fbc683c45219e41c70c6445d759
palluvi66 <palluvi66@gmail.com>
2026-01-08 09:58:00 +0530

    Delete Day-2 directory

--- Day-2/Question-2/index.js
+++
@@ -1,8 +0,0 @@
-import {checkPrime } from "./math.js"
-
-console.log(checkPrime(5));
-console.log(checkPrime(10));
-console.log(checkPrime(15));
-console.log(checkPrime(11));
-console.log(checkPrime(7));
-console.log(checkPrime(13));
