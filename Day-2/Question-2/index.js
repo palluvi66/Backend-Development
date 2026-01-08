2d7d87c284c867878b82ee8c05c52803e952708a
palluvi66 <palluvi66@gmail.com>
2026-01-08 06:33:50 +0530

    Add files via upload

---
+++ Day-2/Question-2/index.js
@@ -0,0 +1,8 @@
+import {checkPrime } from "./math.js"
+
+console.log(checkPrime(5));
+console.log(checkPrime(10));
+console.log(checkPrime(15));
+console.log(checkPrime(11));
+console.log(checkPrime(7));
+console.log(checkPrime(13));
